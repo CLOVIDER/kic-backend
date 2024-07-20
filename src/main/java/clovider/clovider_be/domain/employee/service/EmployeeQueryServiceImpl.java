169137718ae9a633c2fa449d73571871460f96b7
sungsil0624 <ht0729@gachon.ac.kr>
2024-07-20 23:06:00 +0900

    CLO-77 :bug: fix: 서비스 의존관계 수정, 공지사항 조회 로직 추가

--- src/main/java/clovider/clovider_be/domain/employee/service/EmployeeQueryServiceImpl.java
+++ src/main/java/clovider/clovider_be/domain/employee/service/EmployeeQueryServiceImpl.java
@@ -1,5 +1,21 @@
 package clovider.clovider_be.domain.employee.service;
 
-public class EmployeeQueryServiceImpl {
+import clovider.clovider_be.domain.employee.Employee;
+import clovider.clovider_be.domain.employee.repository.EmployeeRepository;
+import clovider.clovider_be.global.exception.ApiException;
+import clovider.clovider_be.global.response.code.status.ErrorStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class EmployeeQueryServiceImpl implements EmployeeQueryService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee findById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new ApiException(ErrorStatus._EMPLOYEE_NOT_FOUND));
+    }
 }
