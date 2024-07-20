1d096556ea30c645f9b6a66980682523fb4be9f3
sungsil0624 <ht0729@gachon.ac.kr>
2024-07-20 23:06:00 +0900

    CLO-77 :bug: fix: 서비스 의존관계 수정, 공지사항 조회 로직 추가

--- src/main/java/clovider/clovider_be/domain/employee/service/EmployeeQueryService.java
+++ src/main/java/clovider/clovider_be/domain/employee/service/EmployeeQueryService.java
@@ -1,5 +1,10 @@
 package clovider.clovider_be.domain.employee.service;
 
-public class EmployeeQueryService {
+import clovider.clovider_be.domain.employee.Employee;
+import org.springframework.stereotype.Service;
 
+@Service
+public interface EmployeeQueryService {
+
+    Employee findById(Long id);
 }
--- src/main/java/clovider/clovider_be/domain/qna/service/QnaCommandServiceImpl.java
+++ src/main/java/clovider/clovider_be/domain/qna/service/QnaCommandServiceImpl.java
@@ -1,13 +1,10 @@
 package clovider.clovider_be.domain.qna.service;
 
 import clovider.clovider_be.domain.common.CustomResult;
-import clovider.clovider_be.domain.employee.Employee;
-import clovider.clovider_be.domain.employee.repository.EmployeeRepository;
+import clovider.clovider_be.domain.employee.service.EmployeeQueryService;
 import clovider.clovider_be.domain.qna.Qna;
 import clovider.clovider_be.domain.qna.dto.QnaRequest;
 import clovider.clovider_be.domain.qna.repository.QnaRepository;
-import clovider.clovider_be.global.exception.ApiException;
-import clovider.clovider_be.global.response.code.status.ErrorStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,19 +13,14 @@ import org.springframework.stereotype.Service;
 public class QnaCommandServiceImpl implements QnaCommandService {
 
     private final QnaRepository qnaRepository;
-    private final EmployeeRepository employeeRepository;
+    private final EmployeeQueryService employeeQueryService;
 
-    private static final String FIXED_EMPLOYEE_USERNAME = "employee";
 
     public CustomResult createQna(QnaRequest qnaRequest) {
-        Employee employee = employeeRepository.findByAccountId(FIXED_EMPLOYEE_USERNAME)
-                .orElseThrow(() -> new ApiException(ErrorStatus._EMPLOYEE_NOT_FOUND));
 
-        Qna savedQna = qnaRepository.save(Qna.builder()
-                .title(qnaRequest.getTitle())
-                .question(qnaRequest.getQuestion())
-                .visibility(qnaRequest.isVisibility())
-                .employee(employee).build());
+        // 2번 데이터로 고정
+        Qna savedQna = qnaRepository.save(QnaRequest
+                .toQna(qnaRequest, employeeQueryService.findById(2L)));
 
         return CustomResult.toCustomResult(savedQna.getId());
     }
