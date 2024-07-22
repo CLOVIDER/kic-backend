2a99ce0af07cfd289471e251e4d8b4f3ed4b685d
정희찬 <anselmo228@naver.com>
2024-07-22 18:01:27 +0900

    feat: S3-CONFIG 및 S3-SERVICE

--- src/main/java/clovider/clovider_be/global/s3/S3Service.java
+++ src/main/java/clovider/clovider_be/global/s3/S3Service.java
@@ -1,5 +1,22 @@
 package clovider.clovider_be.global.s3;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@PropertySource("classpath:application-dev.yml")
 public class S3Service {
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
 
 }
--- src/main/java/clovider/clovider_be/global/s3/S3config.java
+++ src/main/java/clovider/clovider_be/global/s3/S3config.java
@@ -2,7 +2,7 @@ package clovider.clovider_be.global.s3;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,13 +20,13 @@ public class S3config {
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3Client() {
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-        final BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder
+        return (AmazonS3Client) AmazonS3ClientBuilder
             .standard()
-            .withCredentials(new AWSStaticCredentialsProvider(credentials))
             .withRegion(region)
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
             .build();
     }
 
