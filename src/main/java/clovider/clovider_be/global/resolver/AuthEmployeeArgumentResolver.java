fb947bfaf6a065621669460330331c2da103551e
gyeom <rlagusrua3687@gmail.com>
2024-07-22 16:00:35 +0900

    CLO-97 :sparkles: feat: AuthEmployee 어노테이션 구현

---
+++ src/main/java/clovider/clovider_be/global/resolver/AuthEmployeeArgumentResolver.java
@@ -0,0 +1,43 @@
+package clovider.clovider_be.global.resolver;
+
+import clovider.clovider_be.domain.employee.service.EmployeeQueryService;
+import clovider.clovider_be.global.annotation.AuthEmployee;
+import clovider.clovider_be.global.exception.ApiException;
+import clovider.clovider_be.global.response.code.status.ErrorStatus;
+import clovider.clovider_be.global.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class AuthEmployeeArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final EmployeeQueryService employeeQueryService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthEmployee.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new ApiException(ErrorStatus._UNAUTHORIZED);
+        }
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        Long employeeId = Long.parseLong(userDetails.getUsername());
+        return employeeQueryService.getEmployee(employeeId);
+    }
+}
