abb0ae8fec2b6b1e09c750b4bf7111956795c1b5
gyeom <rlagusrua3687@gmail.com>
2024-07-24 13:13:11 +0900

    CLO-109 :sparkles: feat: 회원가입 API 구현
    - 직원 더미데이터 생성 용도

--- src/main/java/clovider/clovider_be/domain/employee/dto/EmployeeRequest.java
+++ src/main/java/clovider/clovider_be/domain/employee/dto/EmployeeRequest.java
@@ -1,6 +1,9 @@
 package clovider.clovider_be.domain.employee.dto;
 
+import clovider.clovider_be.domain.employee.Employee;
+import clovider.clovider_be.domain.enums.Role;
 import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
 import lombok.Getter;
 
 public class EmployeeRequest {
@@ -10,5 +13,18 @@ public class EmployeeRequest {
 
         @NotBlank(message = "비밀번호가 입력되지 않았습니다.")
         private String password;
+    }
+
+    public static Employee toEntity(String accountId, String password) {
+
+        return Employee.builder()
+                .nameKo("EMPLOYEE_1")
+                .accountId(accountId)
+                .password(password)
+                .employeeNo("12345678")
+                .joinDt(LocalDate.of(2024, 7, 1))
+                .dept("IT")
+                .role(Role.EMPLOYEE)
+                .build();
     }
 }
--- src/main/java/clovider/clovider_be/global/auth/controller/AuthController.java
+++ src/main/java/clovider/clovider_be/global/auth/controller/AuthController.java
@@ -4,11 +4,13 @@ import static clovider.clovider_be.domain.common.CustomResult.toCustomResult;
 
 import clovider.clovider_be.domain.common.CustomResult;
 import clovider.clovider_be.domain.employee.Employee;
+import clovider.clovider_be.domain.employee.service.EmployeeCommandService;
 import clovider.clovider_be.domain.employee.service.EmployeeQueryService;
 import clovider.clovider_be.global.auth.dto.AuthRequest.LoginRequest;
 import clovider.clovider_be.global.auth.dto.TokenVo;
 import clovider.clovider_be.global.auth.service.AuthService;
 import clovider.clovider_be.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final EmployeeQueryService employeeQueryService;
+    private final EmployeeCommandService employeeCommandService;
     private final AuthService authService;
 
     @PostMapping("/login")
@@ -32,6 +35,15 @@ public class AuthController {
         TokenVo tokenVo = authService.generateATAndRT(employee);
         authService.setHeaderAccessToken(response, tokenVo.accessToken());
         authService.setHeaderRefreshToken(response, tokenVo.refreshToken());
+
+        return ApiResponse.onSuccess(toCustomResult(employee.getId()));
+    }
+
+    @Operation(summary = "회원가입 - 직원 더미 데이터 생성하는 API", description = "비밀번호를 bcrypt 암호화 적용해서 DB에 저장하기")
+    @PostMapping("/signup")
+    public ApiResponse<CustomResult> singUp(@RequestBody LoginRequest request) {
+
+        Employee employee = employeeCommandService.signUp(request);
 
         return ApiResponse.onSuccess(toCustomResult(employee.getId()));
     }
