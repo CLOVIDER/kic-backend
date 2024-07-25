d1347e5379fe7e3a405446ed602ee274724ff84c
gyeom <rlagusrua3687@gmail.com>
2024-07-25 19:48:24 +0900

    CLO-127 :memo: docs: Lottery 기본 클래스 생성

---
+++ src/main/java/clovider/clovider_be/domain/lottery/service/LotteryQueryService.java
@@ -0,0 +1,10 @@
+package clovider.clovider_be.domain.lottery.service;
+
+import clovider.clovider_be.domain.lottery.dto.CompetitionRate;
+import clovider.clovider_be.domain.recruit.Recruit;
+import java.util.List;
+
+public interface LotteryQueryService {
+
+    List<CompetitionRate> getRecruitRates(List<Recruit> recruits);
+}
