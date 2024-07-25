4c3a9c232c25db649c31ff2a58241b466eccbff1
sungsil0624 <ht0729@gachon.ac.kr>
2024-07-25 16:54:16 +0900

    CLO-77 ✨ feat : api 스웨거 설명 추가

--- src/main/java/clovider/clovider_be/domain/qna/dto/QnaAnswerRequest.java
+++ src/main/java/clovider/clovider_be/domain/qna/dto/QnaAnswerRequest.java
@@ -1,8 +1,10 @@
 package clovider.clovider_be.domain.qna.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class QnaAnswerRequest {
+    @Schema(description = "답변 내용", example = "답변을 여기에 입력하세요.")
     private String answer;
 }
--- src/main/java/clovider/clovider_be/domain/qna/dto/QnaRequest.java
+++ src/main/java/clovider/clovider_be/domain/qna/dto/QnaRequest.java
@@ -2,13 +2,19 @@ package clovider.clovider_be.domain.qna.dto;
 
 import clovider.clovider_be.domain.employee.Employee;
 import clovider.clovider_be.domain.qna.Qna;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class QnaRequest {
 
+    @Schema(description = "질문 제목", example = "시스템 오류 문의", required = true, maxLength = 100)
     private String title;
+
+    @Schema(description = "질문 내용", example = "시스템 오류가 발생했습니다. 도움을 요청합니다.", maxLength = 500)
     private String question;
+
+    @Schema(description = "공지사항의 공개 여부", example = "1", allowableValues = {"0", "1"})
     private Character isVisibility;
 
     public static Qna toQna(QnaRequest qnaRequest, Employee employee) {
