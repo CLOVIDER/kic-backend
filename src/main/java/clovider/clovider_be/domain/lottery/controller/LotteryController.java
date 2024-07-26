f4961530c35de81c022436626957051bd385c4ac
KIM-KWAN-IL <david5452@NAVER.COM>
2024-07-27 00:59:57 +0900

    test

--- src/main/java/clovider/clovider_be/domain/lottery/controller/LotteryController.java
+++ src/main/java/clovider/clovider_be/domain/lottery/controller/LotteryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/admin/lotteries")
 public class LotteryController {
 
+
     private final LotteryService lotteryService;
 
     @Autowired
