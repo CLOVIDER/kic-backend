6d6c80b1c791e765aa1bc13ae312bf82f4b402e0
gyeom <rlagusrua3687@gmail.com>
2024-07-27 19:25:15 +0900

    CLO-133 :sparkles: feat: 관리자 대시보드 조회 API 구현

---
+++ src/main/java/clovider/clovider_be/domain/admin/controller/AdminController.java
@@ -0,0 +1,63 @@
+package clovider.clovider_be.domain.admin.controller;
+
+import static clovider.clovider_be.domain.admin.dto.AdminResponse.DashBoard;
+import static clovider.clovider_be.domain.admin.dto.AdminResponse.toDashBoard;
+
+import clovider.clovider_be.domain.lottery.dto.LotteryResponse.AcceptResult;
+import clovider.clovider_be.domain.lottery.dto.LotteryResponse.CompetitionRate;
+import clovider.clovider_be.domain.lottery.dto.LotteryResponse.TotalApplication;
+import clovider.clovider_be.domain.lottery.service.LotteryQueryService;
+import clovider.clovider_be.domain.notice.dto.NoticeTop3;
+import clovider.clovider_be.domain.notice.service.NoticeQueryService;
+import clovider.clovider_be.domain.qna.service.QnaQueryService;
+import clovider.clovider_be.domain.recruit.Recruit;
+import clovider.clovider_be.domain.recruit.dto.RecruitResponse;
+import clovider.clovider_be.domain.recruit.dto.RecruitResponse.NowRecruitInfo;
+import clovider.clovider_be.domain.recruit.service.RecruitQueryService;
+import clovider.clovider_be.global.response.ApiResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final QnaQueryService qnaQueryService;
+    private final NoticeQueryService noticeQueryService;
+    private final RecruitQueryService recruitQueryService;
+    private final LotteryQueryService lotteryQueryService;
+
+
+    @GetMapping("/dashboards")
+    public ApiResponse<DashBoard> getDashboard() {
+
+        // 진행 중인 모집 정보, 기간, 경쟁률
+        List<Recruit> recruits = recruitQueryService.getNowRecruitPeriod();
+        List<CompetitionRate> recruitRates = lotteryQueryService.getRecruitRates(recruits);
+        NowRecruitInfo nowRecruitInfo = RecruitResponse.toNowRecruitInfo(recruits, recruitRates);
+
+        // 총 신청자 수
+        List<TotalApplication> totalApplication = lotteryQueryService.getTotalApplication(recruits);
+
+        // 승인 대기 수
+        List<TotalApplication> unAcceptApplication = lotteryQueryService.getUnAcceptApplication(
+                recruits);
+
+        // 신청 현황
+        List<AcceptResult> acceptStatus = lotteryQueryService.getAcceptStatus(recruits);
+
+        // Top3 공지글
+        List<NoticeTop3> noticeTop3 = noticeQueryService.getTop3Notices();
+
+        // 답변 대기 수
+        Integer waitQna = qnaQueryService.getWaitQna();
+
+        return ApiResponse.onSuccess(
+                toDashBoard(nowRecruitInfo, totalApplication, unAcceptApplication, acceptStatus,
+                        noticeTop3, waitQna));
+    }
+}
---
+++ src/main/java/clovider/clovider_be/domain/admin/dto/AdminResponse.java
@@ -0,0 +1,43 @@
+package clovider.clovider_be.domain.admin.dto;
+
+import clovider.clovider_be.domain.lottery.dto.LotteryResponse.AcceptResult;
+import clovider.clovider_be.domain.lottery.dto.LotteryResponse.TotalApplication;
+import clovider.clovider_be.domain.notice.dto.NoticeTop3;
+import clovider.clovider_be.domain.recruit.dto.RecruitResponse.NowRecruitInfo;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class AdminResponse {
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class DashBoard {
+
+        private NowRecruitInfo recruitInfo;
+        private List<TotalApplication> totalApplications;
+        private List<TotalApplication> unAcceptApplications;
+        private List<AcceptResult> acceptResults;
+        private List<NoticeTop3> noticeTop3;
+        private Integer waitQnaCnt;
+
+    }
+
+    public static DashBoard toDashBoard(NowRecruitInfo recruitInfo,
+            List<TotalApplication> totalApplications, List<TotalApplication> unAcceptApplications,
+            List<AcceptResult> acceptResults, List<NoticeTop3> noticeTop3, Integer waitQnaCnt) {
+
+        return DashBoard.builder()
+                .recruitInfo(recruitInfo)
+                .totalApplications(totalApplications)
+                .unAcceptApplications(unAcceptApplications)
+                .acceptResults(acceptResults)
+                .noticeTop3(noticeTop3)
+                .waitQnaCnt(waitQnaCnt)
+                .build();
+    }
+}
