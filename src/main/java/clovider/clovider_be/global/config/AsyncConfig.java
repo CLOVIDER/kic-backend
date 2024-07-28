7c405a8bbc9e5abc9b297213fa5603d9ea9d2ddb
gyeom <rlagusrua3687@gmail.com>
2024-07-28 17:43:33 +0900

    CLO-124 :sparkles: feat: 이메일 전송 비동기 처리 적용

---
+++ src/main/java/clovider/clovider_be/global/config/AsyncConfig.java
@@ -0,0 +1,30 @@
+package clovider.clovider_be.global.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(10);
+        executor.setKeepAliveSeconds(30);
+        executor.setThreadNamePrefix("Async MailExecutor");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return AsyncConfigurer.super.getAsyncUncaughtExceptionHandler();
+    }
+}
