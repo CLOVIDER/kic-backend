c9089edbfdd942d1376970cc8e7785e9db9ff89e
sungsil0624 <ht0729@gachon.ac.kr>
2024-07-30 09:11:03 +0900

    CLO-132 ✅ test : 공지사항 레포지토리 키워드 검색 테스트(제목, 내용, both)) 최신 3개 글 조회 테스트 추가

--- src/test/java/clovider/clovider_be/domain/notice/repository/NoticeRepositoryTest.java
+++ src/test/java/clovider/clovider_be/domain/notice/repository/NoticeRepositoryTest.java
@@ -14,6 +14,7 @@ import clovider.clovider_be.domain.noticeImage.NoticeImage;
 import clovider.clovider_be.domain.noticeImage.repository.NoticeImageRepository;
 import clovider.clovider_be.global.config.QuerydslConfig;
 import java.lang.reflect.Field;
+import java.security.PublicKey;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,10 +34,10 @@ class NoticeRepositoryTest {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    private static Employee admin;
+    private Employee admin;
 
     @BeforeEach
-    void setup(){
+    void setup() {
         admin = Employee.builder()
                 .nameKo("홍길동")
                 .accountId("hong123")
@@ -48,39 +49,39 @@ class NoticeRepositoryTest {
                 .build();
 
         employeeRepository.save(admin);
+    }
 
+    private Notice createAndSaveNotice(String title, String content) {
+        Notice notice = Notice.builder()
+                .title(title)
+                .content(content)
+                .admin(admin)
+                .build();
+        return noticeRepository.save(notice);
+    }
+
+    private void addImagesToNotice(Notice notice, List<String> imageUrls) {
+        imageUrls.forEach(url -> {
+            NoticeImage image = NoticeImage.builder()
+                    .image(url)
+                    .notice(notice)
+                    .build();
+            notice.getImages().add(image);
+        });
     }
 
     @Test
     @DisplayName("공지사항 생성 및 조회 테스트")
     public void createNoticeTest() {
         // given
-        Notice notice = Notice.builder()
-                .title("공지사항 제목")
-                .content("공지사항 내용")
-                .admin(admin)
-                .build();
-
-        Notice savedNotice = noticeRepository.save(notice);
-
-        NoticeImage image1 = NoticeImage.builder()
-                .image("http://example.com/image1.jpg")
-                .notice(savedNotice)
-                .build();
-        NoticeImage image2 = NoticeImage.builder()
-                .image("http://example.com/image2.jpg")
-                .notice(savedNotice)
-                .build();
-
-        savedNotice.getImages().add(image1);
-        savedNotice.getImages().add(image2);
+        Notice savedNotice = createAndSaveNotice("공지사항 제목", "공지사항 내용");
+        addImagesToNotice(savedNotice, List.of("http://example.com/image1.jpg", "http://example.com/image2.jpg"));
 
         // when
         Notice resultNotice = noticeRepository.findById(savedNotice.getId()).orElse(null);
 
         // then
         assertNotNull(resultNotice);
-        assertThat(resultNotice.getId()).isNotNull();
         assertThat(resultNotice.getImages()).hasSize(2);
         assertThat(resultNotice.getImages().get(0).getImage()).isEqualTo("http://example.com/image1.jpg");
         assertThat(resultNotice.getImages().get(1).getImage()).isEqualTo("http://example.com/image2.jpg");
@@ -90,22 +91,10 @@ class NoticeRepositoryTest {
     @DisplayName("공지사항 수정 테스트")
     public void updateNoticeImagesTest() throws NoSuchFieldException, IllegalAccessException {
         // given
-        Notice notice = Notice.builder()
-                .title("원래 제목")
-                .content("원래 내용")
-                .admin(admin)
-                .build();
-        Notice savedNotice = noticeRepository.save(notice);
-
-        NoticeImage image1 = NoticeImage.builder()
-                .image("http://example.com/old_image1.jpg")
-                .notice(savedNotice)
-                .build();
-
-        savedNotice.getImages().add(image1);
+        Notice savedNotice = createAndSaveNotice("원래 제목", "원래 내용");
+        addImagesToNotice(savedNotice, List.of("http://example.com/old_image1.jpg"));
 
         NoticeRequest noticeRequest = new NoticeRequest();
-
         Field titleField = NoticeRequest.class.getDeclaredField("title");
         Field contentField = NoticeRequest.class.getDeclaredField("content");
         Field imageUrlsField = NoticeRequest.class.getDeclaredField("imageUrls");
@@ -136,24 +125,8 @@ class NoticeRepositoryTest {
     @DisplayName("공지사항 삭제 테스트")
     public void deleteNoticeTest() {
         // given
-        Notice notice = Notice.builder()
-                .title("공지사항 제목")
-                .content("공지사항 내용")
-                .admin(admin)
-                .build();
-        Notice savedNotice = noticeRepository.save(notice);
-
-        NoticeImage image1 = NoticeImage.builder()
-                .image("http://example.com/image1.jpg")
-                .notice(savedNotice)
-                .build();
-        NoticeImage image2 = NoticeImage.builder()
-                .image("http://example.com/image2.jpg")
-                .notice(savedNotice)
-                .build();
-
-        savedNotice.getImages().add(image1);
-        savedNotice.getImages().add(image2);
+        Notice savedNotice = createAndSaveNotice("공지사항 제목", "공지사항 내용");
+        addImagesToNotice(savedNotice, List.of("http://example.com/image1.jpg", "http://example.com/image2.jpg"));
 
         // when
         noticeRepository.deleteById(savedNotice.getId());
@@ -163,46 +136,14 @@ class NoticeRepositoryTest {
     }
 
     @Test
-    @DisplayName("공지사항 검색 테스트")
-    public void searchNoticeTest() {
+    @DisplayName("공지사항 검색 테스트 - TITLE")
+    public void searchNoticeByTitleTest() {
         // given
-        Notice notice1 = Notice.builder()
-                .title("공지사항 제목")
-                .content("공지사항 내용")
-                .admin(admin)
-                .build();
-        Notice savedNotice = noticeRepository.save(notice1);
+        Notice savedNotice1 = createAndSaveNotice("공지사항 제목", "공지사항 내용");
+        addImagesToNotice(savedNotice1, List.of("http://example.com/image1.jpg", "http://example.com/image2.jpg"));
 
-        NoticeImage image1 = NoticeImage.builder()
-                .image("http://example.com/image1.jpg")
-                .notice(savedNotice)
-                .build();
-        NoticeImage image2 = NoticeImage.builder()
-                .image("http://example.com/image2.jpg")
-                .notice(savedNotice)
-                .build();
-
-        savedNotice.getImages().add(image1);
-        savedNotice.getImages().add(image2);
-
-        Notice notice2 = Notice.builder()
-                .title("공지사항 제목2")
-                .content("공지사항 내용2")
-                .admin(admin)
-                .build();
-        Notice savedNotice2 = noticeRepository.save(notice2);
-
-        NoticeImage image3 = NoticeImage.builder()
-                .image("http://example.com/image3.jpg")
-                .notice(savedNotice)
-                .build();
-        NoticeImage image4 = NoticeImage.builder()
-                .image("http://example.com/image4.jpg")
-                .notice(savedNotice)
-                .build();
-
-        savedNotice2.getImages().add(image3);
-        savedNotice2.getImages().add(image4);
+        Notice savedNotice2 = createAndSaveNotice("공지사항 제목2", "공지사항 내용2");
+        addImagesToNotice(savedNotice2, List.of("http://example.com/image3.jpg", "http://example.com/image4.jpg"));
 
         // when
         List<NoticeResponse> searchedNotices = noticeRepository.searchNotices(SearchType.TITLE, "공지사항");
@@ -213,13 +154,106 @@ class NoticeRepositoryTest {
 
         List<String> titles = searchedNotices.stream()
                 .map(NoticeResponse::getTitle)
-                .collect(Collectors.toList());
+                .toList();
 
-        assertThat(titles).contains("공지사항 제목");
+        assertThat(titles).contains("공지사항 제목", "공지사항 제목2");
 
         assertThat(searchedNotices.stream().anyMatch(n -> n.getNoticeImageList().size() == 2)).isTrue();
-
     }
 
+    @Test
+    @DisplayName("공지사항 검색 테스트 - CONTENT")
+    public void searchNoticeByContentTest() {
+        // given
+        Notice savedNotice1 = createAndSaveNotice("공지사항 제목", "공지사항 내용");
+        addImagesToNotice(savedNotice1, List.of("http://example.com/image1.jpg", "http://example.com/image2.jpg"));
 
+        Notice savedNotice2 = createAndSaveNotice("공지사항 제목2", "공지사항 내용2");
+        addImagesToNotice(savedNotice2, List.of("http://example.com/image3.jpg", "http://example.com/image4.jpg"));
+
+        // when
+        List<NoticeResponse> searchedNotices = noticeRepository.searchNotices(SearchType.CONTENT, "공지사항");
+
+        // then
+        assertThat(searchedNotices).isNotEmpty();
+        assertThat(searchedNotices).hasSize(2);
+
+        List<String> contents = searchedNotices.stream()
+                .map(NoticeResponse::getContent)
+                .toList();
+
+        assertThat(contents).contains("공지사항 내용", "공지사항 내용2");
+
+        assertThat(searchedNotices.stream().anyMatch(n -> n.getNoticeImageList().size() == 2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("공지사항 검색 테스트 - BOTH")
+    public void searchNoticeByBothTest() {
+        // given
+        Notice savedNotice1 = createAndSaveNotice("공지사항 제목 - keyword", "공지사항 내용");
+        addImagesToNotice(savedNotice1, List.of("http://example.com/image1.jpg", "http://example.com/image2.jpg"));
+
+        Notice savedNotice2 = createAndSaveNotice("공지사항 제목2", "공지사항 내용2 - keyword");
+        addImagesToNotice(savedNotice2, List.of("http://example.com/image3.jpg", "http://example.com/image4.jpg"));
+
+        // when
+        List<NoticeResponse> searchedNotices = noticeRepository.searchNotices(SearchType.BOTH, "keyword");
+
+        // then
+        assertThat(searchedNotices).isNotEmpty();
+        assertThat(searchedNotices).hasSize(2);
+
+        List<String> contents = searchedNotices.stream()
+                .map(NoticeResponse::getContent)
+                .toList();
+
+        assertThat(contents).contains("공지사항 내용", "공지사항 내용2 - keyword");
+
+        assertThat(searchedNotices.stream().anyMatch(n -> n.getNoticeImageList().size() == 2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("최신 3개 공지사항 조회 테스트")
+    public void findTop3ByOrderByIdDescTest() {
+        // given
+        Notice notice1 = createAndSaveNotice("Notice 1", "Content 1");
+        addImagesToNotice(notice1, List.of("http://example.com/image1.jpg", "http://example.com/image2.jpg"));
+
+        Notice notice2 = createAndSaveNotice("Notice 2", "Content 2");
+        addImagesToNotice(notice2, List.of("http://example.com/image3.jpg", "http://example.com/image4.jpg"));
+
+        Notice notice3 = createAndSaveNotice("Notice 3", "Content 3");
+        addImagesToNotice(notice3, List.of("http://example.com/image5.jpg", "http://example.com/image6.jpg"));
+
+        Notice notice4 = createAndSaveNotice("Notice 4", "Content 4");
+        addImagesToNotice(notice4, List.of("http://example.com/image7.jpg", "http://example.com/image8.jpg"));
+
+        // when
+        List<Notice> top3Notices = noticeRepository.findTop3ByOrderByIdDesc();
+
+        // then
+        assertThat(top3Notices).hasSize(3);
+        assertThat(top3Notices.get(0).getId()).isGreaterThan(top3Notices.get(1).getId());
+        assertThat(top3Notices.get(1).getId()).isGreaterThan(top3Notices.get(2).getId());
+        assertThat(top3Notices).containsExactly(notice4, notice3, notice2);
+        assertThat(top3Notices.get(0).getImages()).hasSize(2);
+        assertThat(top3Notices.get(1).getImages()).hasSize(2);
+        assertThat(top3Notices.get(2).getImages()).hasSize(2);
+
+        assertThat(top3Notices.get(0).getImages().stream()
+                .map(NoticeImage::getImage)
+                .collect(Collectors.toList()))
+                .containsExactly("http://example.com/image7.jpg", "http://example.com/image8.jpg");
+
+        assertThat(top3Notices.get(1).getImages().stream()
+                .map(NoticeImage::getImage)
+                .collect(Collectors.toList()))
+                .containsExactly("http://example.com/image5.jpg", "http://example.com/image6.jpg");
+
+        assertThat(top3Notices.get(2).getImages().stream()
+                .map(NoticeImage::getImage)
+                .collect(Collectors.toList()))
+                .containsExactly("http://example.com/image3.jpg", "http://example.com/image4.jpg");
+    }
 }
