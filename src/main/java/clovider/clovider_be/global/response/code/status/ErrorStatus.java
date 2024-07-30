f91c102b3dded97de15580989dc0921e4d915863
Chung Hee chan <anselmo228@naver.com>
2024-07-30 14:12:37 +0900

    Update ErrorStatus.java

--- src/main/java/clovider/clovider_be/global/response/code/status/ErrorStatus.java
+++ src/main/java/clovider/clovider_be/global/response/code/status/ErrorStatus.java
@@ -62,7 +62,7 @@ public enum ErrorStatus implements BaseErrorCode {
     _LOTTERY_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "LOTTERY400", "lottery를 찾을 수 없습니다."),
 
     // 모집 관련
-    _RECRUIT_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "RECRUIT400", "recruit를 찾을 수 없습니다.");
+    _RECRUIT_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "RECRUIT400", "recruit를 찾을 수 없습니다."),
 
 
     _EXTERNAL_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "LABMDA400", "LAMBDA 확률 에측 서비스에 연결할 수 없습니다");
