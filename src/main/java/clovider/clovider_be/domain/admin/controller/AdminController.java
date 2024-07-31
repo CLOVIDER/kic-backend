bd9048e322893f87b5fc6fc35d46017ee23fe548
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2024-07-31 21:00:08 +0900

    Clo 125 feat: (관리자) 모집결과 PDF 파일 다운 API 구현 (#62)
    * CLO-125 :sparkles: feat: 각 추첨의 모집 결과 및 모집 정보 조회 쿼리 구현
    
    * CLO-125 :sparkles: feat: html 파일 pdf 변환 서비스 구현
    
    * CLO-125 :sparkles: feat: 모집 결과 PDF 파일 다운 API 구현

--- src/main/java/clovider/clovider_be/domain/admin/controller/AdminController.java
+++ src/main/java/clovider/clovider_be/domain/admin/controller/AdminController.java
@@ -9,6 +9,7 @@ import clovider.clovider_be.domain.application.service.ApplicationQueryService;
 import clovider.clovider_be.domain.common.CustomPage;
 import clovider.clovider_be.domain.lottery.dto.LotteryResponse.AcceptResult;
 import clovider.clovider_be.domain.lottery.dto.LotteryResponse.CompetitionRate;
+import clovider.clovider_be.domain.lottery.dto.LotteryResponse.RecruitInfo;
 import clovider.clovider_be.domain.lottery.dto.LotteryResponse.RecruitResult;
 import clovider.clovider_be.domain.lottery.service.LotteryQueryService;
 import clovider.clovider_be.domain.mail.service.MailService;
@@ -21,18 +22,28 @@ import clovider.clovider_be.domain.recruit.dto.RecruitCreateResponseDTO;
 import clovider.clovider_be.domain.recruit.dto.RecruitResponse;
 import clovider.clovider_be.domain.recruit.dto.RecruitResponse.NowRecruitInfo;
 import clovider.clovider_be.domain.recruit.service.RecruitCommandService;
-import clovider.clovider_be.domain.recruit.service.RecruitCommandServiceImpl;
 import clovider.clovider_be.domain.recruit.service.RecruitQueryService;
 import clovider.clovider_be.global.response.ApiResponse;
+import clovider.clovider_be.global.util.PdfUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "관리자 기능 관련 API 명세서")
 @RestController
@@ -46,6 +57,7 @@ public class AdminController {
     private final LotteryQueryService lotteryQueryService;
     private final MailService mailService;
     private final ApplicationQueryService applicationQueryService;
+    private final PdfUtil pdfUtil;
     private final RecruitCommandService recruitCommandService;
 
 
@@ -119,9 +131,28 @@ public class AdminController {
         return ApiResponse.onSuccess(new CustomPage<>(applicationPage));
     }
 
+    @Operation(summary = "모집 결과 PDF 파일로 다운하기", description = "특정 모집의 결과를 조회하여 PDF 파일을 다운합니다.")
+    @Parameter(name = "recruitId", description = "모집 ID")
+    @PostMapping("/recruits/{recruitId}/export")
+    public ResponseEntity<ByteArrayResource> exportLottery(
+            @PathVariable("recruitId") Long recruitId) {
+
+        List<RecruitResult> recruitResult = lotteryQueryService.getRecruitResult(recruitId);
+        RecruitInfo recruitInfo = recruitQueryService.getRecruitInfo(recruitId);
+
+        byte[] pdfBytes = pdfUtil.resultPdf(recruitResult, recruitInfo);
+
+        ByteArrayResource resource = new ByteArrayResource(pdfBytes);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=result.pdf");
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/pdf");
+
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+    }
 
     @PostMapping("/create/recruit")
-    public ApiResponse<RecruitCreateResponseDTO> createRecruit(@RequestBody RecruitCreateRequestDTO requestDTO) {
+    public ApiResponse<RecruitCreateResponseDTO> createRecruit(
+            @RequestBody RecruitCreateRequestDTO requestDTO) {
         RecruitCreateResponseDTO responseDTO = recruitCommandService.createRecruit(requestDTO);
         return ApiResponse.onSuccess(responseDTO);
     }
