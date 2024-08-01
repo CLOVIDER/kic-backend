e711ba6fa5d8b71ead9af70d3e1d9bf4e7f83217
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2024-08-01 15:16:58 +0900

    CLO-157 :sparkles: feat: 사내 직원 정보 조회 API 구현 (#72)

---
+++ src/main/java/clovider/clovider_be/domain/employee/dto/EmployeeResponse.java
@@ -0,0 +1,36 @@
+package clovider.clovider_be.domain.employee.dto;
+
+import clovider.clovider_be.domain.employee.Employee;
+import clovider.clovider_be.global.util.TimeUtil;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class EmployeeResponse {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class EmployeeInfo {
+
+        private String nameKo;
+        private String accountId;
+        private String employeeNo;
+        private Boolean isCouple;
+        private String workedAt;
+    }
+
+    public static EmployeeInfo toEmployeeInfo(Employee employee) {
+
+        return EmployeeInfo.builder()
+                .nameKo(employee.getNameKo())
+                .accountId(employee.getAccountId())
+                .employeeNo(employee.getEmployeeNo())
+                .isCouple(employee.getCoupleNo() == null)
+                .workedAt(TimeUtil.formattedDate(employee.getCreatedAt()))
+                .build();
+    }
+
+}
