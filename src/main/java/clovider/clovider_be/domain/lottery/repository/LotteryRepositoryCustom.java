855332eafb57e657bb68ecbefbdb32cc34189eed
sungsil0624 <ht0729@gachon.ac.kr>
2024-08-04 18:37:30 +0900

    CLO-175 🐛 fix : conflict 해결

--- src/main/java/clovider/clovider_be/domain/lottery/repository/LotteryRepositoryCustom.java
+++ src/main/java/clovider/clovider_be/domain/lottery/repository/LotteryRepositoryCustom.java
@@ -5,6 +5,8 @@ import clovider.clovider_be.domain.admin.dto.AdminResponse.LotteryResult;
 import clovider.clovider_be.domain.lottery.dto.LotteryResponse.CompetitionRate;
 import clovider.clovider_be.domain.recruit.Recruit;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
