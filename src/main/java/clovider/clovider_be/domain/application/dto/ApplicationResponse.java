b6a287becc2591b0a7f45100e1f83880aa773cde
KWON-MINWOO <117968404+MINUUUUUUUUUUUU@users.noreply.github.com>
2024-08-06 14:46:49 +0900

    CLO-154 ✨Feat : APPLICATION SAVE 상태 관련 수정

--- src/main/java/clovider/clovider_be/domain/application/dto/ApplicationResponse.java
+++ src/main/java/clovider/clovider_be/domain/application/dto/ApplicationResponse.java
@@ -26,16 +26,6 @@ public class ApplicationResponse {
     @JsonIgnore
     private Employee employee;
 
-//     모집 도메인 구현 후 사용
-//     신청서 하나 당 여러 모집에 해당되고 결과도 여러개니 List로 저장
-//    private List<Result> ResultList;
-//    private List<Integer> rankNoList;
-
-    // 어린이집, 모집 도메인 구현 후 사용
-    // (신청서 - 추첨 테이블끼리 조인 -> 추첨 테이블 내 모집ID 를 사용해서 모집 테이블에서 어린이집 ID 얻어옴 -> 어린이집 이름 가져옴)
-    // 신청서 하나 당 여러 어린이집이 해당되니 List로 저장
-//    private List<String> kdgList;
-
     private Integer workYears;
     private Character isSingleParent;
     private Integer ChildrenCnt;
