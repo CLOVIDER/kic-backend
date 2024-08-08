b7acfb4c42b60841bc62eb5e2b7861f0e3a36ce3
KIM-KWAN-IL <david5452@NAVER.COM>
2024-08-08 19:02:13 +0900

    파라미터 description 추가

--- src/main/java/clovider/clovider_be/domain/admin/controller/AdminController.java
+++ src/main/java/clovider/clovider_be/domain/admin/controller/AdminController.java
@@ -228,6 +228,7 @@ public class AdminController {
     }
 
     @Operation(summary = "관리자가 모집을 수정한다.", description = "관리자가 이미 생성된 모집을 수정한다.")
+    @Parameter(name = "recruitId", description = "모집 ID")
     @PatchMapping("/recruit/{recruitId}")
     public ApiResponse<RecruitCreationInfo> updateRecruit(
             @PathVariable Long recruitId,
