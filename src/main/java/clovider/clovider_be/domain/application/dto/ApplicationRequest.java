41286f7b4dadc1d783d1c3d447d347719a57b7a9
KWON-MINWOO <117968404+MINUUUUUUUUUUUU@users.noreply.github.com>
2024-08-08 13:34:18 +0900

    CLO-154 ✨Feat : dto example 추가

--- src/main/java/clovider/clovider_be/domain/application/dto/ApplicationRequest.java
+++ src/main/java/clovider/clovider_be/domain/application/dto/ApplicationRequest.java
@@ -40,9 +40,25 @@ public class ApplicationRequest {
     @Schema(description = "형제 자매 재원 여부 (해당 시 '1', 미해당 시 '0'으로 입력)", example = "1")
     private Character isSibling;
 
-    @Schema(description = "(어린이 : 모집ID) 리스트", example = "")
+    @Schema(description = "(어린이 : 모집ID) 리스트", example = "[\n"
+            + "    {\n"
+            + "      \"childNm\": \"Ali\",\n"
+            + "      \"recruitIds\": [1, 2]\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"childNm\": \"Bob\",\n"
+            + "      \"recruitIds\": [3, 4]\n"
+            + "    }\n"
+            + "  ]")
     private List<Map<String, Object>> childrenRecruitList;
 
-    @Schema(description = "증빙 서류 URL 리스트", example = "['s3-1', 's3-2']")
+    @Schema(description = "증빙 서류 URL 리스트", example = "{\n"
+            + "      \"RESIDENT_REGISTER\": \"s3-1\",\n"
+            + "      \"DUAL_INCOME\": \"s3-2\",\n"
+            + "      \"SINGLE_PARENT\": \"s3-3\",\n"
+            + "      \"DISABILITY\": \"s3-4\",\n"
+            + "      \"MULTI_CHILDREN\": \"s3-5\",\n"
+            + "      \"SIBLING\": \"s3-6\"\n"
+            + "    }")
     private Map<DocumentType, String> imageUrls;
 }
