fcd42282c68ec2b9992395990ac5919119a42fe4
KWON-MINWOO <117968404+MINUUUUUUUUUUUU@users.noreply.github.com>
2024-08-09 16:01:25 +0900

    CLO-216 FEAT : PR 이슈 수용

--- src/main/java/clovider/clovider_be/domain/document/service/ApplicationDocumentCommandService.java
+++ src/main/java/clovider/clovider_be/domain/document/service/ApplicationDocumentCommandService.java
@@ -1,6 +1,7 @@
 package clovider.clovider_be.domain.document.service;
 
 import clovider.clovider_be.domain.application.Application;
+import clovider.clovider_be.domain.enums.Accept;
 import clovider.clovider_be.domain.enums.DocumentType;
 import java.util.List;
 import java.util.Map;
