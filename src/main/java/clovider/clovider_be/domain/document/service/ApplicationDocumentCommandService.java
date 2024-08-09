5229d00b5e4a2dfa840444290a6b0789471cae4c
KWON-MINWOO <117968404+MINUUUUUUUUUUUU@users.noreply.github.com>
2024-08-09 16:01:39 +0900

    CLO-216 ✨Feat : Document 마다 승인여부 속성 추가 및 승인 관련 로직에 적용 (#142)
    * CLO-216 ✨Feat : Document마다 승인여부 속성 추가 및 승인 관련 로직에 적용
    
    * CLO-216 FEAT : 메소드 명 수정 (isAccept -> changeAccept) 및 PR 이슈 수용
    
    * CLO-216 FEAT : PR 이슈 수용

--- src/main/java/clovider/clovider_be/domain/document/service/ApplicationDocumentCommandService.java
+++ src/main/java/clovider/clovider_be/domain/document/service/ApplicationDocumentCommandService.java
@@ -1,10 +1,12 @@
 package clovider.clovider_be.domain.document.service;
 
 import clovider.clovider_be.domain.application.Application;
+import clovider.clovider_be.domain.enums.Accept;
 import clovider.clovider_be.domain.enums.DocumentType;
 import java.util.List;
 import java.util.Map;
 
 public interface ApplicationDocumentCommandService {
-    void createApplicationDocuments(Map<DocumentType, String> fileUrls, Application application);
+    void createApplicationDocuments(Map<DocumentType, String> imageUrls, Application application);
+    void acceptDocument(Long documentId, Accept accept);
 }
--- src/main/java/clovider/clovider_be/domain/document/service/ApplicationDocumentCommandServiceImpl.java
+++ src/main/java/clovider/clovider_be/domain/document/service/ApplicationDocumentCommandServiceImpl.java
@@ -3,11 +3,12 @@ package clovider.clovider_be.domain.document.service;
 import clovider.clovider_be.domain.application.Application;
 import clovider.clovider_be.domain.document.Document;
 import clovider.clovider_be.domain.document.repository.ApplicationDocumentRepository;
+import clovider.clovider_be.domain.enums.Accept;
 import clovider.clovider_be.domain.enums.DocumentType;
+import clovider.clovider_be.global.exception.ApiException;
+import clovider.clovider_be.global.response.code.status.ErrorStatus;
 import jakarta.transaction.Transactional;
 import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -33,5 +34,14 @@ public class ApplicationDocumentCommandServiceImpl implements ApplicationDocumen
                     .build();
             applicationDocumentRepository.save(document);
         });
+    }
+
+    @Override
+    public void acceptDocument(Long documentId, Accept accept){
+        Document document = applicationDocumentRepository.findById(documentId).orElseThrow(
+                () -> new ApiException(ErrorStatus._APPLICATION_NOT_FOUND)
+        );
+
+        document.changeAccept(accept);
     }
 }
