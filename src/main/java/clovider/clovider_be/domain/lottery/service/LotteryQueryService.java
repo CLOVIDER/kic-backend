b26af72535eba16eb51737a46b27de33f897d338
sungsil0624 <ht0729@gachon.ac.kr>
2024-08-10 12:46:18 +0900

    CLO-218 🐛 fix : 공지사항과 qna dto validation 변경, 관리자 추첨 결과 탭 api 포맷 수정

--- src/main/java/clovider/clovider_be/domain/lottery/service/LotteryQueryService.java
+++ src/main/java/clovider/clovider_be/domain/lottery/service/LotteryQueryService.java
@@ -3,10 +3,10 @@ package clovider.clovider_be.domain.lottery.service;
 import clovider.clovider_be.domain.admin.dto.AdminResponse.AcceptResult;
 import clovider.clovider_be.domain.admin.dto.AdminResponse.LotteryResult;
 import clovider.clovider_be.domain.employee.Employee;
+import clovider.clovider_be.domain.enums.AgeClass;
 import clovider.clovider_be.domain.lottery.dto.LotteryResponse.ChildInfo;
 import clovider.clovider_be.domain.lottery.dto.LotteryResponse.CompetitionRate;
 import clovider.clovider_be.domain.lottery.dto.LotteryResponse.RecruitResult;
-import clovider.clovider_be.domain.lottery.dto.LotteryResultByEmployeeDTO;
 import clovider.clovider_be.domain.lottery.dto.LotteryResultResponseDTO;
 import clovider.clovider_be.domain.lottery.dto.LotteryResultsGroupedByChildDTO;
 import clovider.clovider_be.domain.recruit.Recruit;
@@ -34,7 +34,7 @@ public interface LotteryQueryService {
     List<ChildInfo> getChildInfos(Long applicationId);
 
 
-    Page<LotteryResult> getLotteryResultByLotteryId(Long kindergartenId, Pageable pageable, String value);
+    Page<LotteryResult> getLotteryResultByLotteryId(AgeClass ageClass, Long kindergartenId, Pageable pageable, String value);
 
 
 
