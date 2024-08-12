0450e22b07eab1e5d81effc3a447f8bd3b093665
김관일 <118953934+KIM-KWAN-IL@users.noreply.github.com>
2024-08-12 10:41:55 +0900

    FEAT: Clo 208 관리자 모집설정 patch api (#136)
    * 모집생성 결과 DTO형식 통일 완료
    
    * 관리자 모집설정 페이지 PATCH API 구현완료
    
    기존 RecruitCreationInfo DTO 사용
    
    * 파라미터 description 추가
    
    * 여러 어린이집과 그에 따른 분반별로 정원 설정 가능하도록 수정완료
    
    * Revert "Merge branch 'refs/heads/Fix-모집생성결과DTO-통합' into CLO-208-관리자-모집설정-PATCH-API"
    
    This reverts commit c453f5b0e9584ce1c2728b751b59ab8f1fdf9738, reversing
    changes made to b7acfb4c42b60841bc62eb5e2b7861f0e3a36ce3.
    
    * 각 어린이집에따른 분반별로 모집 정원 반환
    
    * 모집 수정 API DTO 수정
    
    * 관리자가 모집을 수정할 수 있는 API
    
    * 어린이집ID 가 아닌 모집ID를 리스트로 받아서 수정
    
    * 모집 수정 DTO 수정
    
    * 모집 수정DTO 단순화
    
    * Update RecruitCommandServiceImpl.java

---
+++ src/main/java/clovider/clovider_be/domain/recruit/dto/RecruitUpdateRequestDTO.java
@@ -0,0 +1,25 @@
+package clovider.clovider_be.domain.recruit.dto;
+
+import clovider.clovider_be.domain.enums.AgeClass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecruitUpdateRequestDTO {
+    private List<Long> recruitIds; // 여러 recruit ID를 포함
+    private AgeClass ageClass;
+    private LocalDateTime recruitStartDt;
+    private LocalDateTime recruitEndDt;
+    private Integer recruitCnt;
+    private LocalDateTime firstStartDt;
+    private LocalDateTime firstEndDt;
+    private LocalDateTime secondStartDt;
+    private LocalDateTime secondEndDt;
+    private RecruitResponse.RecruitWeightInfo recruitWeightInfo;
+}
