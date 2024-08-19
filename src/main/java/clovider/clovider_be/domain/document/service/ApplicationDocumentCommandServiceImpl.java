9d83371355839468f150743a368fded20a0ba0f3
KWON-MINWOO <117968404+MINUUUUUUUUUUUU@users.noreply.github.com>
2024-08-19 10:09:38 +0900

    FIX - 신청서 서류 null 시 빈 값 저장 안되게 수정 (#211)
    * FIX - 신청서 서류 null 시 빈 값 저장 안되게 수정
    
    * FIX - isEmpty() 추가

--- src/main/java/clovider/clovider_be/domain/document/service/ApplicationDocumentCommandServiceImpl.java
+++ src/main/java/clovider/clovider_be/domain/document/service/ApplicationDocumentCommandServiceImpl.java
@@ -25,6 +25,10 @@ public class ApplicationDocumentCommandServiceImpl implements ApplicationDocumen
     public void createApplicationDocuments(Map<DocumentType, String> fileUrls, Application application) {
 
         fileUrls.forEach((documentType, fileUrl) -> {
+            if (fileUrl == null || fileUrl.isEmpty()) {
+                return;
+            }
+
             Document document = Document.builder()
                     .image(fileUrl)
                     .application(application)
