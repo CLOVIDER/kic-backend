65a39c37c6aabda2c61110aa94034d21e4a1093d
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2024-08-20 10:15:27 +0900

    Fix: /api/admin/recruits 기본값 정보 수정 (#223) (#224)
    * :fire: feat: 모든 kindergartenClass 조회
    
    * :fire: feat: 기본 어린이집 모집 설정 정보 조회 구현

--- src/main/java/clovider/clovider_be/domain/kindergartenClass/repository/KindergartenClassRepository.java
+++ src/main/java/clovider/clovider_be/domain/kindergartenClass/repository/KindergartenClassRepository.java
@@ -1,15 +1,21 @@
 package clovider.clovider_be.domain.kindergartenClass.repository;
 
 import clovider.clovider_be.domain.kindergartenClass.KindergartenClass;
-import org.springframework.data.repository.query.Param;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface KindergartenClassRepository extends JpaRepository<KindergartenClass, Long> {
+
     List<KindergartenClass> findByKindergartenId(Long kindergartenId);
+
     void deleteAllByKindergartenId(Long kindergartenId);
 
     @Query("select k.className from KindergartenClass k where k.kindergarten.id = :kindergartenId and k.ageClass = :ageClass")
-    String findClassNameById(@Param("kindergartenId") Long kindergartenId, @Param("ageClass") int ageClass);
+    String findClassNameById(@Param("kindergartenId") Long kindergartenId,
+            @Param("ageClass") int ageClass);
+
+    @Query("select kc from KindergartenClass kc join fetch kc.kindergarten order by kc.ageClass")
+    List<KindergartenClass> findAllWithKindergarten();
 }
